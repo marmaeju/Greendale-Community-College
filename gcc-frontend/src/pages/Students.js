62041c4cbf74259d8d9e31bcaca37ecbdce62192
Madeline A <madeline.ashley2@gmail.com>
2022-11-21 16:34:56 -0600

    Added Students Page -madeline

--- gcc-frontend/src/pages/Students.js
+++ gcc-frontend/src/pages/Students.js
@@ -1,5 +1,52 @@
+import axios from "axios"
+import {useEffect,useState} from 'react'
+import { Navigate, useNavigate } from "react-router-dom"
+
+
+
 const Students = () => {
-  return <div>map out students</div>
+  let navigate=useNavigate()
+  const [studentList,setStudentList]=useState(null)
+  const [selectedStudent,setSelectedStudent]=useState([])
+
+  getAllStudents=async ()=>{
+    const res=await axios.get("localhost:3001/student")
+    console.log(res.data)
+    setStudentList(res.data)
+  }
+
+  const selectStudent=(selected)=>{
+    setSelectedStudent(selected)
+    navigate(`/${selected.id}`)
+  }
+
+  useEffect(()=>{
+    getAllStudents()
+  },[])
+
+  return( 
+  <div className="students-container">Browse All Students
+   {studentList?.map((student)=>
+   <div className="student">
+      <div>First Name:{student.firstName}</div>
+      <div>Last Name:{student.lastName}</div>
+      <div>Email:{student.email}</div>
+      <div>{student.firstName}</div>
+      <div onClick={()=>selectStudent(record)}>
+        <img src={student.image}></img>
+      </div>
+
+
+
+   </div>
+    
+    )}
+
+
+
+  </div>
+  
+  )
 }
 
 export default Students
