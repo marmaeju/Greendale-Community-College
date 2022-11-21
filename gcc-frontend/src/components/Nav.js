1fe948174ab1ce978b819f11f328c857cf8807b5
Marley Judd <marleymaejudd@gmail.com>
2022-11-21 15:30:07 -0700

    nav and homepage filled out

--- gcc-frontend/src/components/Nav.js
+++ gcc-frontend/src/components/Nav.js
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom'
+
 const Nav = () => {
-  return <div>Navbar</div>
+  return (
+    <div>
+      <nav>
+        <h2>Greendale Community College</h2>
+        <Link to="/">Home</Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/students">Students</Link>
+      </nav>
+    </div>
+  )
 }
 
 export default Nav
