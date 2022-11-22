24378cc9b3e8b80428bac073e351ad1a5bda20e0
Danny <dannypeterson28@gmail.com>
2022-11-21 22:40:15 -0500

    styled students

--- gcc-frontend/src/pages/Students.js
+++ gcc-frontend/src/pages/Students.js
@@ -49,20 +49,22 @@ const Students = () => {
       <h1>
         <em>Browse Greendale Students</em>
       </h1>
-      {studentList?.map((student) => (
-        <div className="student" key={student.id}>
-          <h3>
-            {student.lastName},{student.firstName}
-          </h3>
-          <div>
-            <em>{student.email}</em>
+      <div className="students-map">
+        {studentList?.map((student) => (
+          <div className="student" key={student.id}>
+            <h3>
+              {student.lastName},{student.firstName}
+            </h3>
+            <div>
+              <em>{student.email}</em>
+            </div>
+            <div onClick={() => selectStudent(student)}>
+              <img src={student.image} alt="student image"></img>
+            </div>
+            <div>GPA: {student.gpa}</div>
           </div>
-          <div onClick={() => selectStudent(student)}>
-            <img src={student.image} alt="student image"></img>
-          </div>
-          <div>GPA: {student.gpa}</div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
